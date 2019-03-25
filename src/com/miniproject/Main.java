6035da5f9c1394f617927e3f93bce21895ce74eb
Elisha Mayer <44092325+ElishaMayer@users.noreply.github.com>
2019-03-25 13:09:50 +0200

    Add Name

--- src/com/miniproject/Main.java
+++ src/com/miniproject/Main.java
@@ -1,5 +1,11 @@
 package com.miniproject;
 
+/**
+ * Home Work 1.
+ * Written by:
+ * Elisha Mayer ,319185997 ,elisja.mayer@gmail.com .
+ * Menachem Natanson , , .
+ */
 public class Main {
 
     public static void main(String[] args) {
