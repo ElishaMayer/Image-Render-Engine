8131316cdff4bf4b1b6e55bb2f443d98c2f95eb5
MenachemNatanson <48324267+MenachemNatanson@users.noreply.github.com>
2019-04-03 11:36:44 +0300

    Cyliner javadoc + sync

--- src/test/CylinderTest.java
+++ src/test/CylinderTest.java
@@ -10,7 +10,10 @@ import static org.junit.Assert.*;
 
 public class CylinderTest {
 
-    //check Cylinder normal
+    /**
+     * check cylinder normal,
+     * test for {@link Cylinder#getNormal getNormal()} method
+     */
     @Test
     public void getNormal() {
         Cylinder cy = new Cylinder(2,new Ray(new Point3D(2, 0, 0), new Vector(0, 1, 0)),10);
@@ -19,25 +22,37 @@ public class CylinderTest {
         assertEquals(new Vector(0,-1,0).normal(),cy.getNormal(new Point3D(3,-5,0)));
     }
 
-    //check 0 radius
+    /**
+     * check 0 radius,
+     * test for {@link Cylinder#Cylinder(double, Ray, double) Cylinder(double, Ray, double)} constructor
+     */
     @Test(expected = IllegalArgumentException.class)
     public void radiusZeroExc(){
         Cylinder cy = new Cylinder(0,new Ray(new Point3D(1, 2, 3), new Vector(10, 10, 10)),10);
     }
 
-    //check negative radius
+    /**
+     * check negative radius,
+     * test for {@link Cylinder#Cylinder(double, Ray, double) Cylinder(double, Ray, double)} constructor
+     */
     @Test(expected = IllegalArgumentException.class)
     public void radiusNegativeExc(){
         Cylinder cy = new Cylinder(-1,new Ray(new Point3D(1, 2, 3), new Vector(10, 10, 10)),10);
     }
 
-    //check 0 height
+    /**
+     * check 0 height,
+     * test for {@link Cylinder#Cylinder(double, Ray, double) Cylinder(double, Ray, double)} constructor
+     */
     @Test(expected = IllegalArgumentException.class)
     public void heightZeroExc(){
         Cylinder cy = new Cylinder(5,new Ray(new Point3D(1, 2, 3), new Vector(10, 10, 10)),0);
     }
 
-    //check negative height
+    /**
+     * check negative height,
+     * test for {@link Cylinder#Cylinder(double, Ray, double) Cylinder(double, Ray, double)} constructor
+     */
     @Test(expected = IllegalArgumentException.class)
     public void heightNegativeExc(){
         Cylinder cy = new Cylinder(5,new Ray(new Point3D(1, 2, 3), new Vector(10, 10, 10)),-10);
