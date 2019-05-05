a221ef8fe142e7a412c38480cae5ed9ff79786eb
Elisha Mayer <44092325+ElishaMayer@users.noreply.github.com>
2019-05-05 07:50:08 +0300

    Fix Tube Calc

--- src/com/miniproject/Main.java
+++ src/com/miniproject/Main.java
@@ -39,7 +39,7 @@ public class Main {
                     image.writePixel(x, y, 240,c,c);
                 }
                 else if(isGrid(x,y,100,100))
-                    image.writePixel(x,y,255,0,0);
+                    image.writePixel(x,y,0,255,0);
                 else
                     image.writePixel(x,y,0,0,0);
 
